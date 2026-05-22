7c38f44f5d3de928e83fda77d7304a40c2e37589
anothergreb <raphaelgreb02012010@gmail.com>
2026-05-22 10:52:45 +0000

    create the calc.py

--- 06_05_26/atividade_4.py
+++ 06_05_26/atividade_4.py
@@ -3,5 +3,13 @@ idade = int(input("Digite sua idade: "))
 classificacao = None
 
 if idade <= 12 :
-    classificacao = "crianca"
+    classificacao = "criança"
     print(classificacao)
+elif idade > 12 & idade <= 17:
+    classificacao = "adolescente"
+    print(classificacao)
+elif idade > 17 & idade <= 64:
+    classificacao = "adulto"
+    print(classificacao)
+elif idade > 64:
+    
--- 08_05_26/taskmanager.py
+++ 08_05_26/taskmanager.py
@@ -1,12 +1,13 @@
 tarefas = []
-
+ 
 while True:
     print ("\n- - - GERENCIADOR DE TAREFAS ---")
     print ("1. Adicionar Tarefa ")
     print ("2. Listar Tarefas ")
-    print ("3. Sair ")
-    opcao = int(input(" Escolha uma opcao : "))
-
+    print ("3. Remover Tarefa ")
+    print ("4. Sair ")
+    opcao = int(input(" Escolha uma opcao: "))
+ 
     if opcao == 1:
         item = input("Digite a nova tarefa: ")
         tarefas.append(item)
@@ -16,7 +17,15 @@ while True:
         for i, t in enumerate(tarefas, 1):
             print(f"{i}, {t}")
     elif opcao == 3:
-        print (" Encerrando o programa ...")
+        item = input("Qual Tarefa Deseja Remover: ")
+        if item in tarefas:
+            tarefas.remove(item)
+            print("Tarefa removida!")
+        else:
+            print("Tarefa não encontrada!")
+    elif opcao == 4:
+        print ("Encerrando o programa ...")
         break
     else:
-        print(" Opcao invalida !")
+        print("Opcao invalida !")
+        
